e6b7bc22b207ab9bd976cc024c8df6184ecc0b1d
Steven Declercq <Steven.declercq@bosa.fgov.be>
2022-06-02 09:37:55 +0200

    update

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello Steven Declercq, you OpenShift-Developer-bastard"
+		response = "Hello Steven Declercq, you OpenShift-Developer-bastard and beyond"
 	}
 
 	fmt.Fprintln(w, response)
